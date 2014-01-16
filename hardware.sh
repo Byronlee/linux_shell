acfbc8d93a29f6be35a09910b55a3b89328dc28f
jacksongblack <songyuchaoemail@163.com>
2014-01-16 22:55:10 +0800

    修改获取当前目录的方式

--- hardware.sh
+++ hardware.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
 #测试所有硬盘读的速度并输出到文档
 CURRENT_DIR="${PWD}"
+
+function dividing_line {
+echo "----------------------------" >> "${CURRENT_DIR}"/hardware.out
+}
+
 function disk_read   {
+dividing_line
 cd /dev
 for d in $(ls|grep sd);
 do
  echo "测试${d}的读速度" 
- echo "测试${d}的读速度" >> $1/hardware.out
- sudo hdparm -t /dev/${d} >> $1/hardware.out 
+ echo "测试${d}的读速度" >> "${CURRENT_DIR}"/hardware.out
+ sudo hdparm -t /dev/${d} >> "${CURRENT_DIR}"/hardware.out 
 done
+dividing_line
 }
 #测试写速度
 function disk_write {
+dividing_line
 echo $2
 if [ -d "$1" ];then
 cd $1
 echo "测试写速度"
 echo "$2" 
-echo "$2" >> $3/hardware.out
-sudo time dd if=/dev/zero bs=1024 count=1000000 of=/1Gb.file >> $3/hardware.out 2>&1
+echo "$2" >> "${CURRENT_DIR}"/hardware.out
+sudo time dd if=/dev/zero bs=1024 count=1000000 of=/1Gb.file >> "${CURRENT_DIR}"/hardware.out 2>&1
 fi
+dividing_line
 }
 
 function chkError(){
@@ -30,28 +39,32 @@ function chkError(){
 }
 #检查输出文件是否存在
 function chk_file () {
-if [ -f $1/hardware.out ];then
- rm $1/hardware.out
+if [ -f "${CURRENT_DIR}"/hardware.out ];then
+ rm "${CURRENT_DIR}"/hardware.out
 fi
 }
 
 function hard_disk_info () {
+dividing_line
 echo "输出硬盘信息"
 for d in  a b c d e f g h i j k l m n o p q r s t u v w ;
 do
-sudo  hdparm -i "/dev/sd${d}" >> $1/hardware.out 2> /dev/null
+sudo  hdparm -i "/dev/sd${d}" >>  "${CURRENT_DIR}"/hardware.out 2> /dev/null
 done
 echo "输出完成"
+dividing_line
 }
+
 function cpu_info {
-echo " CPU信息" >> $1/hardware.out
-cat /proc/cpuinfo | grep "model name" | uniq |awk -F: '/model name/{printf $2}' >> $1/hardware.out
-echo ""
+dividing_line
+echo " CPU信息" >> "${CURRENT_DIR}"/hardware.out
+cat /proc/cpuinfo | grep "model name" | uniq |awk -F: '/model name/{printf $2}' >> "${CURRENT_DIR}"/hardware.out
+dividing_line
 }
- "${CURRENT_DIR}"
-chk_file  "${CURRENT_DIR}"  
-disk_read "${CURRENT_DIR}"  
-disk_write /data "测试阵列卡写速度" "${CURRENT_DIR}"  
-disk_write ~  "测试系统盘写速度" "${CURRENT_DIR}"  
-hard_disk_info "${CURRENT_DIR}"  
-cpu_info "${CURRENT_DIR}"  
+chk_file    
+disk_read   
+disk_write /data "测试阵列卡写速度"   
+disk_write ~  "测试系统盘写速度"   
+hard_disk_info   
+cpu_info   
+dividing_line 
